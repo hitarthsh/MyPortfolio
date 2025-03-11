e278191d05fda951a1e29185698f78d765adf3a5
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-03-12 00:43:57 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,8 +39,8 @@ export default function Contact() {
     {
       icon: MapPinIcon,
       title: "Location",
-      content: "Rajkot, Gujarat, India",
-      link: "https://maps.google.com/?q=Rajkot,Gujarat,India",
+      content: "Ahmedabad, Gujarat, India",
+      link: "https://maps.google.com/?q=Ahmedabad,Gujarat,India",
     },
   ];
 
